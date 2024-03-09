fcc55597aeff443ccfbb725175cf955d72120d98
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-03-09 18:38:23 +0900

    Title: 블럭쌓는 명령2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240309/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,7 +1,7 @@
 n, k=tuple(map(int, input().split()))
-li=[0]*(n+1)
+li=[0]*(n)
 for i in range(k):
     a, b=map(int, input().split())
-    for j in range(a, b+1):
+    for j in range(a-1, b):
         li[j]+=1
 print(max(li))
