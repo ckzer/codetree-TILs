704c8f03d48a928d08268796d37e2c0eec5fd398
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-07-30 20:31:42 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+    cin.get();
+
+    cout << "010" << "-" << c << "-" << b;
+    return 0;
+}
