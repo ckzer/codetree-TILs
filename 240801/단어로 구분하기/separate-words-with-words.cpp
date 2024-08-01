46732b0ade26fd5e96f63742ab38dc7121272f00
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-08-01 20:09:22 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr;
+    for(int i=0;i<10;i++){
+        cin>>arr;
+        cout<<arr<<"\n";
+    }
+    return 0;
+}
