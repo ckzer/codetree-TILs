2596d805befe394dfebbc2b48b367ab5c56a7a04
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-08-06 21:00:18 +0900

    Title: 특정 문자의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/특정 문자의 유무/specific-character-presence.cpp
+++ 240806/특정 문자의 유무/specific-character-presence.cpp
@@ -5,12 +5,10 @@ using namespace std;
 int main() {
     string s, first="No", second="No";
     cin >> s;
-    for(int i=0;i<s.length()-1;i++) {
-        if(s.find("ee") != string::npos) {
-            first="Yes";
-        }
-        else if(s.find("ab") != string::npos)   second="Yes";
+    if(s.find("ee") != string::npos) {
+        first="Yes";
     }
+    if(s.find("ab") != string::npos)   second="Yes";
     cout << first << " " << second;
 
     return 0;
