1f5b0eac6d98f306598d6f40437dcec734b8ac4d
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-08-14 18:11:19 +0900

    Title: 특정 문자의 등장 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240814/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
+++ 240814/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
@@ -6,9 +6,9 @@ int main() {
     string cnt;
     int first=0, second=0;
     cin>>cnt;
-    for(int i=0;i<cnt.length()-2;i++) {
-        if(cnt[i]=="e" && cnt[i+1]=="b") first++;
-        else if(cnt[i]=="e" && cnt[i+1]=="e") second++;
+    for(int i=0;i<cnt.length()-1;i++) {
+        if(cnt[i]=='e' && cnt[i+1]=='b') first++;
+        else if(cnt[i]=='e' && cnt[i+1]=='e') second++;
     }
     cout<<first<<" "<<second;
     return 0;
