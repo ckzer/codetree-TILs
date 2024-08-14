bd35d27752d1cfebbab767a44d5693d1e500eae2
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-08-14 18:09:18 +0900

    Title: 특정 문자의 등장 횟수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240814/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
+++ 240814/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
@@ -6,7 +6,7 @@ int main() {
     string cnt;
     int first, second;
     cin>>cnt;
-    for(int i=0;i<cnt.length()-1;i++) {
+    for(int i=0;i<cnt.length()-2;i++) {
         if(cnt[i]=="e" && cnt[i+1]=="b") first++;
         else if(cnt[i]=="e" && cnt[i+1]=="e") second++;
     }
