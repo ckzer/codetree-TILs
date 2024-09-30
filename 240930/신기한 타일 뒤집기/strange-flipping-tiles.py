80aac967a774ded86625ecc4b3c0d7ed1edb424a
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-09-30 23:36:11 +0900

    Title: 신기한 타일 뒤집기, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240930/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240930/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,18 +1,23 @@
+li=[0]*10000
+now = 5000
+white, black = 0, 0
 n=int(input())
-white=0
-black=0
 
 for i in range(n):
     x, direction = input().split()
     x=int(x)
     if direction=='L':
-        white+=x
-        black-=x
-        if black<0:
-            black=0
+        for j in range(now, now-x, -1):
+            li[j]=1
+        now-=x+1
     else:
-        black+=x
-        white-=x
-        if white<0:
-            white=0
+        for j in range(now, now+x):
+            li[j]=-1
+        now+=x-1
+for i in range(10000):
+    if li[i]==-1:
+        black+=1
+    elif li[i]==1:
+        white+=1
+
 print(white, black)
