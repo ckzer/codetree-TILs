c4d5643b491a825e3c4a8e56863514d39411f4e1
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-10-05 01:50:55 +0900

    Title: 흰검 칠하기, Time: 277ms, Memory: 40MB, Status: Passed - CodeTree

--- 241005/흰검 칠하기/painting-white-black.py
+++ 241005/흰검 칠하기/painting-white-black.py
@@ -1,29 +1,29 @@
 n=int(input())
 OFFSET=100000
-check=[0]*(2*OFFSET+1)
+check=[[0, 0] for _ in range(OFFSET*2+1)] #왼쪽이 흰, 오른쪽 검
 color=[0]*(2*OFFSET+1)  #검정색은 2, 흰색은 1
 now = OFFSET
 for i in range(n):
     x, direction = input().split()
     x=int(x)
-    if direction=='L':  
+    if direction=='L':  #흰색
         for j in range(now, now-x, -1):
-            if color[j]==1 or check[j]==4:
+            if check[j][0]>=2 and check[j][1]>=2:
                 continue
-            check[j]+=1
+            check[j][0]+=1
             color[j]=1
         now-=x-1
-    elif direction=='R':
+    elif direction=='R':    #검정색
         for j in range(now, now+x):
-            if color[j]==2 or check[j]==4:
+            if check[j][0]>=2 and check[j][1]>=2:
                 continue
-            check[j]+=1
+            check[j][1]+=1
             color[j]=2
         now+=x-1
 
 black, white, grey = 0, 0, 0
 for i in range(200001):
-    if check[i]>=4:
+    if check[i][0]>=2 and check[i][1]>=2:
         grey+=1
     else:
         if color[i]==2:
