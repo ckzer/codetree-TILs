c0b1ec831aee0d0bcf916558dfd3344e8b177ea5
Junghoon <89171817+ckzer@users.noreply.github.com>
2024-10-19 14:42:31 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241019/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241019/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,20 +1,25 @@
-N, K, P, T = map(int, input().split())
+N, K, P, T = map(int, input().split());
 
-arrN = [0] * (N + 1)
+arrN = [0]*(N+1)
 arrN[P] = 1
-cntN = [0] * (N + 1)
+cntN = [0]*(N+1)
 
 arr = []
-for _ in range(T):
+for i in range(T):
     arr.append(list(map(int, input().split())))
-arr.sort(key=lambda x: x[0])
+arr.sort(key = lambda x: [x[0]])
 
-for t, x, y in arr:
-    if arrN[x] == 1 and cntN[x] < K:
-        arrN[y] = 1
-        cntN[x] += 1
-    if arrN[y] == 1 and cntN[y] < K:
-        arrN[x] = 1
-        cntN[y] += 1
+for i in range(T):
+    x, y = arr[i][1], arr[i][2]
+    if arrN[x] == 1:
+        if cntN[x] < K:
+            arrN[y] = 1
+            cntN[x] += 1
+        
+    else:
+        if arrN[y] == 1:
+            if cntN[y] < K:
+                arrN[x] = 1
+                cntN[y] += 1
 
 print(''.join(map(str, arrN[1:])))
